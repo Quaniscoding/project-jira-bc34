3d5b62afe9e57dd752dd2bddace1fed19be897ee
Ngo Van Quan <vq.2509.2003@gmail.com>
2022-11-25 23:01:36 +0700

    set-up

---
+++ src/utils/baseUrl.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { getStringLocal } from "./config";
+import { DOMAIN_BE, USER_LOGIN } from "./constant";
+
+export const http = axios.create({
+    baseURL: DOMAIN_BE,
+    timeout: 5000
+})
+http.interceptors.request.use((config) => {
+    return {
+        ...config,
+        headers: {
+            ...config.headers,
+            tokenCybersoft: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyOCIsIkhldEhhblN0cmluZyI6IjI1LzAyLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY3NzI4MzIwMDAwMCIsIm5iZiI6MTY0Nzk2ODQwMCwiZXhwIjoxNjc3NDMwODAwfQ.wEdmkKpVZbDB4s4L_cmLwJ1O8le8Cc-VMgLZCI-HvLA",
+            Authorization: `Bearer ${getStringLocal(USER_LOGIN)}`
+        }
+    }
+}, err => { console.log(err) })
